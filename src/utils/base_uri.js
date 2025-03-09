f0c766e3807a3afd1ce9ba4252ccbc927e24c584
sbn raju <simhardrinagaraju@gmail.com>
2025-03-09 17:04:57 +0530

    Vercel configuration

--- src/utils/base_uri.js
+++ src/utils/base_uri.js
@@ -1,3 +1,3 @@
-// const BASE_URI = "http://localhost:8080/api/v1.AdWall";
-const BASE_URI = " https://098d-2401-4900-889d-e668-50b9-5135-c46-b4b3.ngrok-free.app/api/v1.AdWall";
+const BASE_URI = "http://localhost:8080/api/v1.AdWall";
+// const BASE_URI = " https://098d-2401-4900-889d-e668-50b9-5135-c46-b4b3.ngrok-free.app/api/v1.AdWall";
 export default BASE_URI;
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,5 @@ export default defineConfig({
   ],
   server: {
     allowedHosts: ['.ngrok-free.app'],
-    cors: {
-      origin: ["http://localhost:5173", "https://098d-2401-4900-889d-e668-50b9-5135-c46-b4b3.ngrok-free.app"],
-      credentials: true,
-    },
   },
 })
